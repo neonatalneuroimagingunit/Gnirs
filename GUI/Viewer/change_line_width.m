787ac517305a57687ef885d796ddf2d0073cfd8d
Matteo Caffini <mcaffini@users.noreply.github.com>
2018-07-04 10:44:50 +0200

    fix viewer add spectrum

--- GUI/Viewer/change_line_width.m
+++ GUI/Viewer/change_line_width.m
@@ -1,4 +1,4 @@
-function change_line_width(hObject, eventData, channelColors, linewidthCurrent, linewidthFactor, hplot, hmetadata)
+function change_line_width(hObject, eventData, channelColors, linewidthCurrent, linewidthFactor, hplot, hmetadata, fs)
 
 % Change LineWidth in raw_explorer main plot
 %
@@ -8,15 +8,37 @@ function change_line_width(hObject, eventData, channelColors, linewidthCurrent, 
 %
 
 all_lines = findall(gca,'Type','line');
+n_lines = length(all_lines);
+nchannels = size(channelColors, 1);
 colorBackground = [200 200 200]/255;
 colorForeground = flipud(channelColors);
-alphaBackground = 0.4;
+alphaBackground = 0.3;
 alphaForeground = 1;
 
-n_lines = length(all_lines);
+start = int32(hplot.bigaxes2.XLim(1));
+stop = int32(hplot.bigaxes2.XLim(2));
 
-start = hplot.bigaxes2.XLim(1);
-stop = hplot.bigaxes2.XLim(2);
+if start < 1
+    start = 1;
+end
+if stop > str2double(hmetadata(1).value(3).String)
+    stop = int32(str2double(hmetadata(1).value(3).String));
+end
+
+xdataavg = all_lines.XData;
+xdataavg = xdataavg(start:stop);
+ydataavg = mean(eventData.Source.YData(start:stop)) * ones([1 length(xdataavg)]);
+
+xdata = all_lines.XData;
+xdata = xdata(start:stop);
+ydata = eventData.Source.YData(start:stop); % sample frequency (Hz)
+%t = 0:1/fs:10-1/fs;                      % 10 second span time vector
+spectrum = fft(ydata);
+n = length(xdata);          % number of samples
+f = (0:n-1)*(fs/n);     % frequency range
+power = abs(spectrum).^2/n;    % power of the DFT
+power = smooth(power, 500);
+power = power/max(power);
 
 linewidthForeground = linewidthFactor*linewidthCurrent;
 linewidthBackground = linewidthCurrent;
@@ -35,7 +57,15 @@ if get(hObject, 'Type') == 'line'
             hmetadata(4).value(3).String = num2str(1);
             hmetadata(4).value(2).String = [num2str(round(10*mean(eventData.Source.YData))/10) ' ' char(177) ' ' num2str(round(10*std(eventData.Source.YData))/10)];
             hmetadata(4).value(1).String = [num2str(round(10*mean(eventData.Source.YData(start:stop)))/10) ' ' char(177) ' ' num2str(round(10*std(eventData.Source.YData(start:stop)))/10)];
-            %plot(mean(eventData.Source.YData(start:stop)))
+            
+            %hplot.avg1 = plot(xdataavg, ydataavg, 'Color', [1 0 0], 'LineWidth', 6);
+            %plot(f,  power, 'Parent', hplot.spectrumaxes)
+            plot(f,  power, 'Parent', hplot.spectrumaxes, 'Color', [1 0 0], 'LineWidth', 1)
+            hplot.spectrumaxes.XLim = [0 5];
+            hplot.spectrumaxes.YLim = [0 0.1];
+            hplot.spectrumaxes.Color = [1 1 1 0];
+            hplot.spectrumaxes.Box = 'off';
+            hplot.spectrumaxes.Visible = 'on';
         else
             set(all_lines, 'LineWidth', linewidthBackground)
             for ii = 1:1:n_lines
