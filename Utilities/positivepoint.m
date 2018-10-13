ee8667fec67782f1c77bb6a7352c824f0a0b450a
Nadir Pagno <PagnoNadir@gmail.com>
2018-10-13 23:44:21 +0200

    Debug 105 maker

--- Utilities/positivepoint.m
+++ Utilities/positivepoint.m
@@ -5,7 +5,7 @@ linecoff = repmat(linecoff,[size(points,1),1]);
 pointover = (dot(points  - lineinters, linecoff,2) > 0);
 
 distance = vecnorm(points - lineinters, 2, 2);
-pointover = pointover & (distance / max(distance) > 0.9 );
+pointover = pointover & (distance / max(distance(pointover)) > 0.9 );
 
 end
 
