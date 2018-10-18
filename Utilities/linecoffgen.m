a36f9397f7f9409252e7bf90306c64298b8a8e26
Nadir Pagno <PagnoNadir@gmail.com>
2018-10-18 14:28:16 +0200

    fix some Atlas bug

--- Utilities/linecoffgen.m
+++ Utilities/linecoffgen.m
@@ -4,14 +4,10 @@ function [linecoff,lineinters] = linecoffgen(plaincoff, point0, pointA, N)
 % Detailed explanation goes here
 	lineinters = repmat(point0,[N,1]);
 
+	[line1,~] = point2line(pointA,point0);
 
-	line1(1) = 1;
-	line1(2) = (pointA(2)-point0(2))/(pointA(1)-point0(1));
-	line1(3)= (pointA(3)-point0(3))/(pointA(1)-point0(1));
-	
+
 	line2 = cross(line1,plaincoff(1:3));
-	
-	line1 = line1./vecnorm(line1);
 	line2 = line2./vecnorm(line2);
 	
 	line1 = repmat(line1,[N,1]);
