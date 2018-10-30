398416d8f1d2eb461b9dad3554e4284852887cab
Nadir <PagnoNadir@gmail.com>
2018-10-30 22:26:26 +0100

    Add BlackMagic  2 generate Atlas Tag

--- Utilities/Copy_of_atlas105maker.m
+++ Utilities/Copy_of_atlas105maker.m
@@ -124,151 +124,137 @@ secondaryFrontBackPoints = tenFive.coord(:,end - 11 + 1,:);
 % 
 % %% Landmarks names for main circumferences
 % % Left
-tenFive.names(1:5:end,1) = {'Nz'; 'N1h'; 'N1'; 'AFp9'; 'AF9'; 'AFF9'; 'F9'; 'FFT9'; 'FT9'; 'FTT9'; ...
-    'T9'; 'TTP9'; 'TP9'; 'TPP9'; 'P9'; 'PPO9'; 'PO9'; 'POO9'; 'I1'; 'I1h'; 'Iz'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, Iz, T9 , Nz, nStep);
-% tenFive.coord(:,1,:) = pointsrelpos(tempPoints, tempLength, 1:(-0.05):0);
-% 
-tenFive.names(1:5:end,6) = {'NFpz'; 'NFp1h'; 'NFp1'; 'AFp9h'; 'AF9h'; 'AFF9h'; 'F9h'; 'FFT9h'; 'FT9h'; 'FTT9h'; ...
-    'T9h'; 'TTP9h'; 'TP9h'; 'TPP9h'; 'P9h'; 'PPO9h'; 'PO9h'; 'POO9h'; 'O1h'; 'OI1h'; 'OIz'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, OIz, T9h , NFpz, nStep);
-% tenFive.coord(:,2,:) = pointsrelpos(tempPoints, tempLength, 1:(-0.05):0);
-% 
-tenFive.names(1:5:end,11) = {'Fpz'; 'Fp1h'; 'Fp1'; 'AFp7'; 'AF7'; 'AFF7'; 'F7'; 'FFT7'; 'FT7'; 'FTT7'; ...
-    'T7'; 'TTP7'; 'TP7'; 'TPP7'; 'P7'; 'PPO7'; 'PO7'; 'POO7'; 'O1'; 'O1h'; 'Oz'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, Oz, T7 , FPz, nStep);
-% tenFive.coord(:,3,:) = pointsrelpos(tempPoints, tempLength, 1:(-0.05):0);
-% 
-% % Right
-tenFive.names(1:5:end,101) = {'Nz'; 'N2h'; 'N2'; 'AFp10'; 'AF10'; 'AFF10'; 'F10'; 'FFT10'; 'FT10'; 'FTT10'; ...
-    'T10'; 'TTP10'; 'TP10'; 'TPP10'; 'P10'; 'PPO10'; 'PO10'; 'POO10'; 'I2'; 'I2h'; 'Iz'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, Iz, T10 , Nz, nStep);
-% tenFive.coord(:,21,:) = pointsrelpos(tempPoints, tempLength, 1:-0.05:0);
-% 
-tenFive.names(1:5:end,96) = {'NFpz'; 'NFp2h'; 'NFp2'; 'AFp10h'; 'AF10h'; 'AFF10h'; 'F10h'; 'FFT10h'; 'FT10h'; 'FTT10h'; ...
-    'T10h'; 'TTP10h'; 'TP10h'; 'TPP10h'; 'P10h'; 'PPO10h'; 'PO10h'; 'POO10h'; 'O2h'; 'OI2h'; 'OIz'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, OIz, T10h , NFpz, nStep);
-% tenFive.coord(:,20,:) = pointsrelpos(tempPoints, tempLength, 1:-0.05:0);
-% 
- tenFive.names(1:5:end,91) = {'Fpz'; 'Fp2h'; 'Fp2'; 'AFp8'; 'AF8'; 'AFF8'; 'F8'; 'FFT8'; 'FT8'; 'FTT8'; ...
-    'T8'; 'TTP8'; 'TP8'; 'TPP8'; 'P8'; 'PPO8'; 'PO8'; 'POO8'; 'O2'; 'O2h'; 'Oz'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, Oz, T8 , FPz, nStep);
-% tenFive.coord(:,19,:) = pointsrelpos(tempPoints, tempLength, 1:-0.05:0);
-% LoadingBarAtlas.LoadingPerc = 0.5;
+
+
 %% Landmarks for arcs
 arcPerc =  0.9875: - 0.0125 :0.0125;
-% arcPerc = 0.9375:-0.0625:0.0625
-%AFp8 = secondaryFrontBackPoints(4,:);
-%AFp7 = secondaryBackFrontPoints(18,:);
 
 for ii = 12 : 1 : 90 %(101 - 11)
 	LoadingBarAtlas.loadingPerc = LoadingBarAtlas.loadingPerc + dPerc;
 	[tempPoints, ~, tempLength] = pathonmesh(meshPoints, secondaryFrontBackPoints(ii,:), main_BackFront_Points(end - ii + 1,:) , secondaryBackFrontPoints(end - ii + 1,:), nStep);
 	tenFive.coord(ii,12:90,:) = pointsrelpos(tempPoints, tempLength, arcPerc);
 end
+tempNameMatrix = repmat({''},105,105);
+
+tempname = repmat( {'Nz'; 'N1h'; 'N1'; 'AFp9'; 'AF9'; 'AFF9'; 'F9'; 'FFT9'; 'FT9'; 'FTT9'; ...
+    'T9'; 'TTP9'; 'TP9'; 'TPP9'; 'P9'; 'PPO9'; 'PO9'; 'POO9'; 'I1'; 'I1h'; 'Iz'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(:,1:1:5) = tempname;
+
+tempname = repmat({'NFpz'; 'NFp1h'; 'NFp1'; 'AFp9h'; 'AF9h'; 'AFF9h'; 'F9h'; 'FFT9h'; 'FT9h'; 'FTT9h'; ...
+    'T9h'; 'TTP9h'; 'TP9h'; 'TPP9h'; 'P9h'; 'PPO9h'; 'PO9h'; 'POO9h'; 'O1h'; 'OI1h'; 'OIz'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(:,6:1:10) = tempname;
+
+tempname = repmat({'Fpz'; 'Fp1h'; 'Fp1'; 'AFp7'; 'AF7'; 'AFF7'; 'F7'; 'FFT7'; 'FT7'; 'FTT7'; ...
+    'T7'; 'TTP7'; 'TP7'; 'TPP7'; 'P7'; 'PPO7'; 'PO7'; 'POO7'; 'O1'; 'O1h'; 'Oz'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(:,11:1:15) = tempname;
+
+tempname = repmat({'Nz'; 'N2h'; 'N2'; 'AFp10'; 'AF10'; 'AFF10'; 'F10'; 'FFT10'; 'FT10'; 'FTT10'; ...
+    'T10'; 'TTP10'; 'TP10'; 'TPP10'; 'P10'; 'PPO10'; 'PO10'; 'POO10'; 'I2'; 'I2h'; 'Iz'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(:,101:1:105) = tempname;
+
+tempname = repmat({'NFpz'; 'NFp2h'; 'NFp2'; 'AFp10h'; 'AF10h'; 'AFF10h'; 'F10h'; 'FFT10h'; 'FT10h'; 'FTT10h'; ...
+    'T10h'; 'TTP10h'; 'TP10h'; 'TPP10h'; 'P10h'; 'PPO10h'; 'PO10h'; 'POO10h'; 'O2h'; 'OI2h'; 'OIz'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(:,96:1:100) = tempname;
+
+tempname = repmat({'Fpz'; 'Fp2h'; 'Fp2'; 'AFp8'; 'AF8'; 'AFF8'; 'F8'; 'FFT8'; 'FT8'; 'FTT8'; ...
+    'T8'; 'TTP8'; 'TP8'; 'TPP8'; 'P8'; 'PPO8'; 'PO8'; 'POO8'; 'O2'; 'O2h'; 'Oz'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(:,91:1:95) = tempname;
+
+tempname = repmat({'AFp7h'; 'AFp5'; 'AFp5h'; 'AFp3'; 'AFp3h'; 'AFp1'; 'AFp1h'; ...
+    'AFpz'; 'AFp2h'; 'AFp2'; 'AFp4h'; 'AFp4'; 'AFp6h'; 'AFp6'; 'AFp8h'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(16:1:20,16:1:90) = tempname';
+ 
+tempname = repmat({'AF7h'; 'AF5'; 'AF5h'; 'AF3'; 'AF3h'; 'AF1'; 'AF1h'; ...
+    'AFz'; 'AF2h'; 'AF2'; 'AF4h'; 'AF4'; 'AF6h'; 'AF6'; 'AF8h'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(21:1:25,16:1:90) = tempname';
+
+tempname = repmat({'AFF7h'; 'AFF5'; 'AFF5h'; 'AFF3'; 'AFF3h'; 'AFF1'; 'AFF1h'; ...
+    'AFFz'; 'AFF2h'; 'AFF2'; 'AFF4h'; 'AFF4'; 'AFF6h'; 'AFF6'; 'AFF8h'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(26:1:30,16:1:90) = tempname';
+
+tempname = repmat({'F7h'; 'F5'; 'F5h'; 'F3'; 'F3h'; 'F1'; 'F1h'; ...
+    'Fz'; 'F2h'; 'F2'; 'F4h'; 'F4'; 'F6h'; 'F6'; 'F8h'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(31:1:35,16:1:90) = tempname';
+ 
+tempname = repmat({'FFT7h'; 'FFC5'; 'FFC5h'; 'FFC3'; 'FFC3h'; 'FFC1'; 'FFC1h'; ...
+    'FFCz'; 'FFC2h'; 'FFC2'; 'FFC4h'; 'FFC4'; 'FFC6h'; 'FFC6'; 'FFT8h'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(36:1:40,16:1:90) = tempname';
+
+tempname = repmat({'FT7h'; 'FC5'; 'FC5h'; 'FC3'; 'FC3h'; 'FC1'; 'FC1h'; ...
+    'FCz'; 'FC2h'; 'FC2'; 'FC4h'; 'FC4'; 'FC6h'; 'FC6'; 'FT8h'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(41:1:45,16:1:90) = tempname';
+
+tempname = repmat({'FTT7h'; 'FCC5'; 'FCC5h'; 'FCC3'; 'FCC3h'; 'FCC1'; 'FCC1h'; ...
+    'FCCz'; 'FCC2h'; 'FCC2'; 'FCC4h'; 'FCC4'; 'FCC6h'; 'FCC6'; 'FTT8h'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(46:1:50,16:1:90) = tempname';
+
+tempname = repmat({'T7h'; 'C5'; 'C5h'; 'C3'; 'C3h'; 'C1'; 'C1h'; ...
+    'Cz'; 'C2h'; 'C2'; 'C4h'; 'C4'; 'C6h'; 'C6'; 'T8h'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(51:1:55,16:1:90) = tempname';
+
+tempname = repmat({'TTP7h'; 'CCP5'; 'CCP5h'; 'CCP3'; 'CCP3h'; 'CCP1'; 'CCP1h'; ...
+    'CCPz'; 'CCP2h'; 'CCP2'; 'CCP4h'; 'CCP4'; 'CCP6h'; 'CCP6'; 'TTP8h'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(56:1:60,16:1:90) = tempname';
+ 
+tempname = repmat({'TP7h'; 'CP5'; 'CP5h'; 'CP3'; 'CP3h'; 'CP1'; 'CP1h'; ...
+    'CPz'; 'CP2h'; 'CP2'; 'CP4h'; 'CP4'; 'CP6h'; 'CP6'; 'TP8h'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(61:1:65,16:1:90) = tempname';
+
+tempname = repmat({'TPP7h'; 'CPP5'; 'CPP5h'; 'CPP3'; 'CPP3h'; 'CPP1'; 'CPP1h'; ...
+    'CPPz'; 'CPP2h'; 'CPP2'; 'CPP4h'; 'CPP4'; 'CPP6h'; 'CPP6'; 'TPP8h'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(66:1:70,16:1:90) = tempname';
+
+tempname = repmat({'P7h'; 'P5'; 'P5h'; 'P3'; 'P3h'; 'P1'; 'P1h'; ...
+    'Pz'; 'P2h'; 'P2'; 'P4h'; 'P4'; 'P6h'; 'P6'; 'P8h'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(71:1:75,16:1:90) = tempname';
+
+tempname = repmat({'PPO7h'; 'PPO5'; 'PPO5h'; 'PPO3'; 'PPO3h'; 'PPO1'; 'PPO1h'; ...
+    'PPOz'; 'PPO2h'; 'PPO2'; 'PPO4h'; 'PPO4'; 'PPO6h'; 'PPO6'; 'PPO8h'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(76:1:80,16:1:90) = tempname';
+
+tempname = repmat({'PO7h'; 'PO5'; 'PO5h'; 'PO3'; 'PO3h'; 'PO1'; 'PO1h'; ...
+    'POz'; 'PO2h'; 'PO2'; 'PO4h'; 'PO4'; 'PO6h'; 'PO6'; 'PO8h'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(81:1:85,16:1:90) = tempname';
+
+tempname = repmat({'POO7h'; 'POO5'; 'POO5h'; 'POO3'; 'POO3h'; 'POO1'; 'POO1h'; ...
+    'POOz'; 'POO2h'; 'POO2'; 'POO4h'; 'POO4'; 'POO6h'; 'POO6'; 'POO8h'}',5,1);
+tempname = repmat(tempname(:),1,5);
+tempNameMatrix(86:1:90,16:1:90) = tempname';
 
 
 
 
+postFixMatrix = {'_L2F2', '_L1F2', '_F2', '_R1F2', '_R2F2';...
+    '_L2F1', '_L1F1', '_F1', '_R1F1', '_R2F1';...
+    '_L2', '_L1', '', '_Rv1', '_R2';...
+    '_L2B1', '_L1B1', '_B1', '_R1B1', '_R2B1';...
+    '_L2B2', '_L1B2', '_B2', '_R1B2', '_R2B2'}; % left 2 right
+
+postFixMatrix = repmat(postFixMatrix,[21,21]);
+tempNameMatrixWhitPostFix = strcat(tempNameMatrix,postFixMatrix);
+tempNameMatrixWhitPostFix(cellfun('isempty',tempNameMatrix)) = {''};
 
 
-
-tenFive.names(16,16:5:86) = {'AFp7h'; 'AFp5'; 'AFp5h'; 'AFp3'; 'AFp3h'; 'AFp1'; 'AFp1h'; ...
-    'AFpz'; 'AFp2h'; 'AFp2'; 'AFp4h'; 'AFp4'; 'AFp6h'; 'AFp6'; 'AFp8h'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, AFp8, AFpz , AFp7, nStep);
-% tenFive.coord(4,4:18,:) = pointsrelpos(tempPoints, tempLength, arcPerc);
-
-tenFive.names(21,16:5:86) = {'AF7h'; 'AF5'; 'AF5h'; 'AF3'; 'AF3h'; 'AF1'; 'AF1h'; ...
-    'AFz'; 'AF2h'; 'AF2'; 'AF4h'; 'AF4'; 'AF6h'; 'AF6'; 'AF8h'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, AF8, AFz , AF7, nStep);
-% tenFive.coord(5,4:18,:) = pointsrelpos(tempPoints, tempLength, arcPerc);
-
-tenFive.names(26,16:5:86) = {'AFF7h'; 'AFF5'; 'AFF5h'; 'AFF3'; 'AFF3h'; 'AFF1'; 'AFF1h'; ...
-    'AFFz'; 'AFF2h'; 'AFF2'; 'AFF4h'; 'AFF4'; 'AFF6h'; 'AFF6'; 'AFF8h'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, AFF8, AFFz , AFF7, nStep);
-% tenFive.coord(6,4:18,:) = pointsrelpos(tempPoints, tempLength, arcPerc);
-
-tenFive.names(31,16:5:86) = {'F7h'; 'F5'; 'F5h'; 'F3'; 'F3h'; 'F1'; 'F1h'; ...
-    'Fz'; 'F2h'; 'F2'; 'F4h'; 'F4'; 'F6h'; 'F6'; 'F8h'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, F8, Fz , F7, nStep);
-% tenFive.coord(7,4:18,:) = pointsrelpos(tempPoints, tempLength, arcPerc);
-
-tenFive.names(36,16:5:86) = {'FFT7h'; 'FFC5'; 'FFC5h'; 'FFC3'; 'FFC3h'; 'FFC1'; 'FFC1h'; ...
-    'FFCz'; 'FFC2h'; 'FFC2'; 'FFC4h'; 'FFC4'; 'FFC6h'; 'FFC6'; 'FFT8h'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, FFT8, FFCz , FFT7, nStep);
-% tenFive.coord(8,4:18,:) = pointsrelpos(tempPoints, tempLength, arcPerc);
-
-tenFive.names(41,16:5:86) = {'FT7h'; 'FC5'; 'FC5h'; 'FC3'; 'FC3h'; 'FC1'; 'FC1h'; ...
-    'FCz'; 'FC2h'; 'FC2'; 'FC4h'; 'FC4'; 'FC6h'; 'FC6'; 'FT8h'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, FT8, FCz , FT7, nStep);
-% tenFive.coord(9,4:18,:) = pointsrelpos(tempPoints, tempLength, arcPerc);
-
-tenFive.names(46,16:5:86) = {'FTT7h'; 'FCC5'; 'FCC5h'; 'FCC3'; 'FCC3h'; 'FCC1'; 'FCC1h'; ...
-    'FCCz'; 'FCC2h'; 'FCC2'; 'FCC4h'; 'FCC4'; 'FCC6h'; 'FCC6'; 'FTT8h'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, FTT8, FCCz , FTT7, nStep);
-% tenFive.coord(10,4:18,:) = pointsrelpos(tempPoints, tempLength, arcPerc);
-
-% main Cz arc
-tenFive.names(51,16:5:86) = {'T7h'; 'C5'; 'C5h'; 'C3'; 'C3h'; 'C1'; 'C1h'; ...
-    'Cz'; 'C2h'; 'C2'; 'C4h'; 'C4'; 'C6h'; 'C6'; 'T8h'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, T8, Cz , T7, nStep);
-% tenFive.coord(11,4:18,:) = pointsrelpos(tempPoints, tempLength, arcPerc);
-
-%dsfsd
-tenFive.names(56,16:5:86) = {'TTP7h'; 'CCP5'; 'CCP5h'; 'CCP3'; 'CCP3h'; 'CCP1'; 'CCP1h'; ...
-    'CCPz'; 'CCP2h'; 'CCP2'; 'CCP4h'; 'CCP4'; 'CCP6h'; 'CCP6'; 'TTP8h'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, TTP8, CCPz , TTP7, nStep);
-% tenFive.coord(12,4:18,:) = pointsrelpos(tempPoints, tempLength, arcPerc);
-
-tenFive.names(61,16:5:86) = {'TP7h'; 'CP5'; 'CP5h'; 'CP3'; 'CP3h'; 'CP1'; 'CP1h'; ...
-    'CPz'; 'CP2h'; 'CP2'; 'CP4h'; 'CP4'; 'CP6h'; 'CP6'; 'TP8h'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, TP8, CPz , TP7, nStep);
-% tenFive.coord(13,4:18,:) = pointsrelpos(tempPoints, tempLength, arcPerc);
-
-tenFive.names(66,16:5:86) = {'TPP7h'; 'CPP5'; 'CPP5h'; 'CPP3'; 'CPP3h'; 'CPP1'; 'CPP1h'; ...
-    'CPPz'; 'CPP2h'; 'CPP2'; 'CPP4h'; 'CPP4'; 'CPP6h'; 'CPP6'; 'TPP8h'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, TPP8, CPPz , TPP7, nStep);
-% tenFive.coord(14,4:18,:) = pointsrelpos(tempPoints, tempLength, arcPerc);
-
-tenFive.names(71,16:5:86) = {'P7h'; 'P5'; 'P5h'; 'P3'; 'P3h'; 'P1'; 'P1h'; ...
-    'Pz'; 'P2h'; 'P2'; 'P4h'; 'P4'; 'P6h'; 'P6'; 'P8h'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, P8, Pz, P7, nStep);
-% tenFive.coord(15,4:18,:) = pointsrelpos(tempPoints, tempLength, arcPerc);
-
-tenFive.names(76,16:5:86) = {'PPO7h'; 'PPO5'; 'PPO5h'; 'PPO3'; 'PPO3h'; 'PPO1'; 'PPO1h'; ...
-    'PPOz'; 'PPO2h'; 'PPO2'; 'PPO4h'; 'PPO4'; 'PPO6h'; 'PPO6'; 'PPO8h'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, PPO8, PPOz , PPO7, nStep);
-% tenFive.coord(16,4:18,:) = pointsrelpos(tempPoints, tempLength, arcPerc);
-
-tenFive.names(81,16:5:86) = {'PO7h'; 'PO5'; 'PO5h'; 'PO3'; 'PO3h'; 'PO1'; 'PO1h'; ...
-    'POz'; 'PO2h'; 'PO2'; 'PO4h'; 'PO4'; 'PO6h'; 'PO6'; 'PO8h'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, PO8, POz , PO7, nStep);
-% tenFive.coord(17,4:18,:) = pointsrelpos(tempPoints, tempLength, arcPerc);
-
-tenFive.names(86,16:5:86) = {'POO7h'; 'POO5'; 'POO5h'; 'POO3'; 'POO3h'; 'POO1'; 'POO1h'; ...
-    'POOz'; 'POO2h'; 'POO2'; 'POO4h'; 'POO4'; 'POO6h'; 'POO6'; 'POO8h'};
-% [tempPoints, ~, tempLength] = pathonmesh(meshPoints, POO8, POOz , POO7, nStep);
-% tenFive.coord(18,4:18,:) = pointsrelpos(tempPoints, tempLength, arcPerc);
-
-l1 = {'L2'; 'L1'; ''; 'R1'; 'R2'}; % left 2 right
-l2 = {'F2'; 'F1'; ''; 'B1'; 'B2'}; % front 2 back
-separatore = repmat({'_'},[5,5]);
-separatore{3,3} = '';
-
-for i = 6:5:96
-    for j = 6:5:96
-        if ~isempty(tenFive.names(i,j))
-            spam = repmat(tenFive.names(i,j),[5,5]);
-            for x = 1:1:5
-                for y = 1:1:5
-                    spam{x,y} = [spam{x,y} separatore{x,y} l1{x} l2{y}];
-                end
-            end
-            tenFive.names(i-2:i+2,j-2:j+2) = spam;
-        end
-    end
-end
-
-
+tenFive.names = tempNameMatrixWhitPostFix(3:103,3:103);
 end
 
